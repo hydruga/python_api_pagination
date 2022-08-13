7073648c346de1b35dc32a39e0366150ed4720bb
Joshua <99271034+hydruga@users.noreply.github.com>
2022-08-13 10:39:42 -0400

    Python Api Function
    Gets count of all male and female characters from Rick and Morty .

---
+++ getCharactersGender.py
@@ -0,0 +1,34 @@
+import requests
+
+
+def getCharactersGender():
+    baseurl = 'https://rickandmortyapi.com/api/character' 
+    r = requests.get(baseurl)
+    d = r.json()
+    pages = d['info']['pages']
+    item_count=0
+    male=0
+    female=0
+    for count in range(0, pages):
+        print("Accessing page ", count+1, "of pages ", pages)
+        data = r.json()
+        for item in data['results']:
+            name = data['results'][item_count]['name']
+            gender = data['results'][item_count]['gender']
+            if gender == 'Female':
+                female+=1
+            else:
+                male+=1
+            item_count+=1
+        # Count starts at zero but pages counts from 1.
+        if count < pages-1:
+            # Once we've gone through all items on page, reassign request to next page.
+            r = requests.get(data['info']['next'])
+            item_count = 0             # Reset item count
+
+
+    print("Total male characters in Rick and Morty ", male)
+    print("Total female characters in Rick and Morty ", female)
+
+getCharacters()
+    
